719b10cf88987acf1e17cc6fecf15569b5583313
Mehul Pillai <mehulpillai30@gmail.com>
2021-04-23 17:24:20 -0400

    Creates app component

--- app/src/main/java/com/mehul/woons/WoonsApplication.kt
+++ app/src/main/java/com/mehul/woons/WoonsApplication.kt
@@ -1,9 +1,13 @@
 package com.mehul.woons
 
 import android.app.Application
+import com.mehul.woons.DI.AppComponent
+import com.mehul.woons.DI.DaggerAppComponent
 
-class WoonsApplication: Application() {
+class WoonsApplication : Application() {
+    lateinit var appComponent: AppComponent
     override fun onCreate() {
         super.onCreate()
+        appComponent = DaggerAppComponent.builder().application(this).build()
     }
 }
