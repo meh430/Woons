d7ba2c72286a0153453de0408a7c00b7f641acf3
Mehul Pillai <mehulpillai30@gmail.com>
2021-04-23 17:40:57 -0400

    Adds generic loaders

--- app/src/main/java/com/mehul/woons/Utils.kt
+++ app/src/main/java/com/mehul/woons/Utils.kt
@@ -1,4 +1,32 @@
 package com.mehul.woons
 
-class Utils {
-}
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.liveData
+import androidx.lifecycle.map
+import com.mehul.woons.entities.Resource
+import kotlinx.coroutines.Dispatchers
+
+fun <T> loadLocalData(localCall: suspend () -> LiveData<T>): LiveData<Resource<T>> =
+    liveData(Dispatchers.IO) {
+        emit(Resource.loading(null))
+        val result = kotlin.runCatching { localCall().map { Resource.success(it) } }
+        result.onSuccess {
+            emitSource(it)
+        }
+        result.onFailure {
+            emit(Resource.error(it.message, null))
+        }
+    }
+
+
+fun <T> loadRemoteData(remoteCall: suspend () -> T): LiveData<Resource<T>> =
+    liveData(Dispatchers.IO) {
+        emit(Resource.loading(null))
+        val result = kotlin.runCatching { remoteCall() }
+        result.onSuccess {
+            emit(Resource.success(it))
+        }
+        result.onFailure {
+            emit(Resource.error(it.message, null))
+        }
+    }
