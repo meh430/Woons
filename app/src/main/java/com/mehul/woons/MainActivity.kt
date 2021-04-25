b16f92e4dc906ca4a418adbaf45259b105bc24a2
Mehul Pillai <mehulpillai30@gmail.com>
2021-04-25 13:08:04 -0400

    Set up main activity

--- app/src/main/java/com/mehul/woons/MainActivity.kt
+++ app/src/main/java/com/mehul/woons/MainActivity.kt
@@ -1,9 +1,17 @@
 package com.mehul.woons
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.mehul.woons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     /*val lol = object : ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             @Suppress("UNCHECKED_CAST")
@@ -16,8 +24,37 @@ class MainActivity : AppCompatActivity() {
     }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.elevation = 0.0F
+        setupNavigation()
+    }
+
+
+    private fun setupNavigation() {
+        val navController = findNavController(R.id.fragNavHost)
+        binding.bottomNavView.setupWithNavController(navController)
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.browseFragment) {
+                binding.bottomNavView.visibility = View.GONE
+            } else {
+                binding.bottomNavView.visibility = View.VISIBLE
+            }
+        }
+
+        val appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(
+                R.id.libraryFragment,
+                R.id.discoverFragment,
+                R.id.settingsFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.fragNavHost)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
