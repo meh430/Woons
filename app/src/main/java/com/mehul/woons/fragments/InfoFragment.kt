767c513dc667795e06365855c7c07ed1e074dbb6
Mehul Pillai <mehulpillai30@gmail.com>
2021-04-25 19:15:40 -0400

    Adds info fragment

---
+++ app/src/main/java/com/mehul/woons/fragments/InfoFragment.kt
@@ -0,0 +1,33 @@
+package com.mehul.woons.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.mehul.woons.databinding.FragmentInfoBinding
+
+
+class InfoFragment : Fragment() {
+    private var _binding: FragmentInfoBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentInfoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    /*override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Do all observe stuff here
+    }*/
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
