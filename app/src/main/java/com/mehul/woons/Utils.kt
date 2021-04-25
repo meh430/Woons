47d9a53c163713c0762c7875f081aac02943491c
Mehul Pillai <mehulpillai30@gmail.com>
2021-04-25 11:14:34 -0400

    Adds some layouts

--- app/src/main/java/com/mehul/woons/Utils.kt
+++ app/src/main/java/com/mehul/woons/Utils.kt
@@ -1,11 +1,14 @@
 package com.mehul.woons
 
+import android.content.Context
+import android.util.DisplayMetrics
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.liveData
 import androidx.lifecycle.map
 import com.mehul.woons.entities.Resource
 import kotlinx.coroutines.Dispatchers
+
 
 fun <T> loadLocalData(localCall: suspend () -> LiveData<T>): LiveData<Resource<T>> =
     liveData(Dispatchers.IO) {
@@ -34,4 +37,13 @@ fun <T> loadRemoteData(remoteCall: suspend () -> T): LiveData<Resource<T>> =
 
 fun <T> MutableLiveData<T>.notifyObserver() {
     this.value = this.value
+}
+
+fun calculateNoOfColumns(
+    context: Context,
+    columnWidthDp: Float
+): Int { // For example columnWidthdp=180
+    val displayMetrics: DisplayMetrics = context.resources.displayMetrics
+    val screenWidthDp = displayMetrics.widthPixels / displayMetrics.density
+    return (screenWidthDp / columnWidthDp + 0.5).toInt()
 }
