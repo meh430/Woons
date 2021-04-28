700436fd9aa8058f4f2202fd92b7a4f065d895c4
Mehul Pillai <mehulpillai30@gmail.com>
2021-04-28 16:06:12 -0400

    Change color depending on whether read

--- app/src/main/java/com/mehul/woons/adapters/ChapterAdapter.kt
+++ app/src/main/java/com/mehul/woons/adapters/ChapterAdapter.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.mehul.woons.R
 import com.mehul.woons.databinding.ChapterItemBinding
 import com.mehul.woons.databinding.ChapterLoadingBinding
 import com.mehul.woons.databinding.ChapterSheetBinding
@@ -100,9 +102,17 @@ class ChapterAdapter(val listener: ChapterAdapter.ChapterItemListener) :
     inner class ChapterViewHolder(val binding: ChapterItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(chapter: Chapter, position: Int) {
+            // change color to indicate whether read or not
+            val itemTextColor = ContextCompat.getColor(
+                binding.root.context,
+                if (chapter.hasRead) android.R.color.darker_gray else R.color.textColor
+            )
+
             // bind text
             binding.apply {
+                chapterName.setTextColor(itemTextColor)
                 chapterName.text = chapter.chapterNumber
+                uploadDate.setTextColor(itemTextColor)
                 uploadDate.text = chapter.uploadDate
             }
 
